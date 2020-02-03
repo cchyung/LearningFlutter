d611454150bcd617cb709e4723484de60843280f
cchyung <chyungspice@gmail.com>
2020-02-02 16:03:09 -0800

    initial commit of tutorial project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Dog Rating App
+Trying to learn Flutter! Following the https://flutterbyexample.com/ tutorial.
+
+## Progress
+[X] Basic Dogs App Setup
+[X] Data Model & HTTP
+[X] Reusable Custom Card Widget
+[ ] ListView and Builder Methods
+[ ] Gradients Background
+[ ] Routing 1: Pages on the Fly
+[ ] Routing 2: Add New Dog Page
+[ ] Forms 1: User Input
+[ ] Forms 2: Sliders and Buttons
+[ ] User Feedback: Toasts / Snackbars
+[ ] Built In Animation
+[ ] Hero Transition
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import 'dog_model.dart';
+
+import 'dog_card.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'We Rate Dogs',
+      theme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      home: MyHomePage(title: 'We Rate Dogs'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Dog> initialDoggos = []
+    ..add(Dog('Ruby', 'Portland, OR, USA',
+        'Ruby is a very good girl. Yes: Fetch, loungin\'. No: Dogs who get on furniture.'))
+    ..add(Dog('Rex', 'Seattle, WA, USA', 'Best in Show 1999'))
+    ..add(Dog('Rod Stewart', 'Prague, CZ',
+        'Star good boy on international snooze team.'))
+    ..add(Dog('Herbert', 'Dallas, TX, USA', 'A Very Good Boy'))
+    ..add(Dog('Buddy', 'North Pole, Earth', 'Self proclaimed human lover.'));
+
+  
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        backgroundColor: Colors.black87,
+      ),
+      body: Container(
+        child: DogCard(initialDoggos[1]),
+      ),
+    );
+  }
+}
